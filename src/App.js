aaff1592d0a4aa2dcfaa903c48add6760408009b
jgnunes1 <jgnunes@gmail.com>
2023-04-25 20:20:18 -0300

    problema

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
 import Banner from './componentes/Banner/Banner';
+import CampoTexto from './componentes/CampoTexto';
 
 function App() {
   return (
     <div className="App">
       <Banner  />
+      <CampoTexto label="Nome"/>
+      <CampoTexto label="Cargo"/>
+      <CampoTexto label="Imagem"/>
     </div>
   );
 }
