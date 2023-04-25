8ab78aaaa6e21d7aa1b1c69aa88b25f5da3aae51
jgnunes1 <jgnunes@gmail.com>
2023-04-25 17:24:41 -0300

    banner e css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Banner from './componentes/Banner/Banner';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Bem Vindos!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          UERJ 2023
-        </a>
-      </header>
+      <Banner  />
     </div>
   );
 }
