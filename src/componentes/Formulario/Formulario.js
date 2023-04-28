e7dad8f1c2bb2a8fcb941d7ec4e2164d17b23a2b
jgnunes1 <jgnunes@gmail.com>
2023-04-28 17:31:52 -0300

    update formulario

---
+++ src/componentes/Formulario/Formulario.js
@@ -0,0 +1,19 @@
+
+import CampoTexto from '../CampoTexto'
+import './Formulario.css'
+//aerofuntion para que as informações do Formulario vão para index.js
+export const Formulario = () => {
+  //JSX
+  return(
+        <section>
+
+            <form>
+                <h2> Preencha os dados para criar os cards dos funcionários</h2>
+                <CampoTexto label="Nome" placeholder="Digite seu nome"/>
+                <CampoTexto label="Cargo" placeholder="Digite seu cargo"/>
+                <CampoTexto label="Imagem" placeholder="Cole aqui o endereço da imagem"/>
+            </form>
+
+        </section>
+  )
+}
