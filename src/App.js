f32f3d10b9545c75dec4ef486aa0fbff7fc7c2d3
jgnunes1 <jgnunes@gmail.com>
2023-05-09 16:56:54 -0300

    time cores

--- src/App.js
+++ src/App.js
@@ -64,9 +64,9 @@ function App() {
     
     <div className="App">
       <Banner/>
-      <Formulario aoColaboradorCadastrado={ colaborador => aoNovoColaboradorAdicionado(colaboradores) }/>
+      <Formulario aoColaboradorCadastrado = {colaborador => aoNovoColaboradorAdicionado(colaboradores) }/>
     
-      {times.map(time => <Time key={time.nome} nome={time.nome} corPrimaria={time.corPrimaria} corSecundaria={time.corSecundaria} />})
+      {times.map(time => <Time key={time.nome} nome={time.nome} corPrimaria={time.corPrimaria} corSecundaria={time.corSecundaria} />)}
          
     
     </div>
--- src/componentes/Time/Time.js
+++ src/componentes/Time/Time.js
@@ -1,11 +1,13 @@
 import './Time.css'
 
 
-export const Time = { props } => {
+export const Time = (props) => {
     const css = { backgroundColor: props.corSecundaria }
 
     return (
-        <section className='time' style={css}>
-            <h3>style={{ borderColor: props.corPrimaria }}>{props.nome}</h3>
+        <section className="time" style={css}>
+            <h3 style={{ borderColor: props.corPrimaria }}> {props.nome}</h3>
         </section>
     )
+
+}
