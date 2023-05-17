4a99725dcf2b24eb78992cbd09c8e9fd7a7dec57
jgnunes1 <jgnunes@gmail.com>
2023-05-17 20:21:03 -0300

    gerando tsconfig

--- src/componentes/Banner/Banner.js
+++ src/componentes/Banner/Banner.js
@@ -1,10 +1,17 @@
+import { Fragment } from 'react'
 import './Banner.css'
 //aerofuntion para que as informações do Banner vão para index.js
 export const Banner = () => {
   //JSX
   return(
-    <header className="banner">
-        <img src="/imagens/banner.png" />
-    </header>
+
+    <>
+
+      <header className="banner">
+        <img src="/imagens/banner.png" alt="O banner principal da Página" />
+      </header>
+
+    </>
+   
   )
 }
--- src/componentes/Botao/Botao.tsx
+++ src/componentes/Botao/Botao.tsx
@@ -1,6 +1,17 @@
+import { ReactElement } from 'react'
 import './Botao.css'
+
+import React from 'react';
+
+
+interface BotaoProps {
+
+    children: ReactElement
+
+}
+
 //aerofuntion para que as informações do Banner vão para index.js
-export const Botao= (props) => {
+export const Botao= (props: BotaoProps) => {
   //JSX
   return(<button className='botao'>
         {props.children}
