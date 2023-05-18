63816e5094f0588b859987a3a16565920b22ab22
jgnunes1 <jgnunes@gmail.com>
2023-05-18 20:58:57 -0300

    descontrução de tipos

---
+++ src/componentes/Banner/Banner.tsx
@@ -0,0 +1,24 @@
+import { Fragment } from 'react'
+import './Banner.css'
+
+import React from 'react'
+interface BannerProps {
+    enderecoImagem:string
+    textoAlernativo?:string
+}
+//aerofuntion para que as informações do Banner vão para index.js
+export const Banner = ({enderecoImagem, textoAlernativo}:BannerProps) => {
+  //JSX
+  return(
+
+    <>
+
+      <header className="banner">
+       {/* <img src="/imagens/banner.png" alt="O banner principal da Página" />*/}
+        <img src={enderecoImagem} alt={textoAlernativo} />
+      </header>
+
+    </>
+   
+  )
+}
